7390088ab4d1d6199c7c560c956fb946fcd05394
Brun04 <bruno.verchere04@orange.fr>
2021-01-04 15:14:01 +0100

    Add constructor unit tests for LineString

---
+++ src/test/java/org/acme/geometry/LineStringTest.java
@@ -0,0 +1,33 @@
+package org.acme.geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LineStringTest {
+
+	public static final double EPSILON = 1.0e-15;
+	
+	@Test
+	public void testDefaultConstructor(){
+		LineString l = new LineString();
+		Assert.assertEquals(0, l.getNumPoints());
+	}
+	
+	@Test
+	public void testConstructor(){
+		List<Point> points = new ArrayList<Point>();
+		points.add(new Point(new Coordinate(1.0, -2.5)));
+		points.add(new Point());
+		LineString l = new LineString(points);
+		Assert.assertEquals(2, l.getNumPoints());
+		
+		
+		Assert.assertEquals(1.0, l.getPointN(0).getCoordinate().getX(), EPSILON);
+		Assert.assertEquals(-2.5, l.getPointN(0).getCoordinate().getY(), EPSILON);
+		Assert.assertEquals(0.0, l.getPointN(1).getCoordinate().getX(), EPSILON);
+		Assert.assertEquals(0.0, l.getPointN(1).getCoordinate().getY(), EPSILON);
+	}
+}
