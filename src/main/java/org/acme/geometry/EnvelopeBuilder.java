78a89e9686c79e9da76f6a4b9386be9d5bd38518
Brun04 <bruno.verchere04@orange.fr>
2021-01-04 16:48:26 +0100

    Add envelope (0.6)

---
+++ src/main/java/org/acme/geometry/EnvelopeBuilder.java
@@ -0,0 +1,40 @@
+package org.acme.geometry;
+
+public class EnvelopeBuilder {
+	private Envelope bbox;
+	
+	
+	public EnvelopeBuilder() {
+		this.bbox = new Envelope();
+	}
+	
+	public void insert(Coordinate coordinate) {
+		if(this.bbox.isEmpty()) {
+			this.bbox = new Envelope(coordinate, coordinate);
+		}else {
+			if(coordinate.getX() < this.bbox.getXmin() && coordinate.getY() < this.bbox.getYmin()) {
+				this.bbox = new Envelope(coordinate, new Coordinate(this.bbox.getXmax(), this.bbox.getYmax()));
+			}
+			if(coordinate.getX() < this.bbox.getXmin() && coordinate.getY() > this.bbox.getYmin()) {
+				this.bbox = new Envelope(new Coordinate(coordinate.getX(), this.bbox.getYmin()), new Coordinate(this.bbox.getXmax(), this.bbox.getYmax()));
+			}
+			if(coordinate.getX() > this.bbox.getXmin() && coordinate.getY() < this.bbox.getYmin()) {
+				this.bbox = new Envelope(new Coordinate(this.bbox.getXmin(), coordinate.getY()), new Coordinate(this.bbox.getXmax(), this.bbox.getYmax()));
+			}
+			
+			if(coordinate.getX() > this.bbox.getXmax() && coordinate.getY() > this.bbox.getYmax()) {
+				this.bbox = new Envelope(new Coordinate(this.bbox.getXmin(), this.bbox.getYmin()), coordinate);
+			}
+			if(coordinate.getX() > this.bbox.getXmax() && coordinate.getY() < this.bbox.getYmax()) {
+				this.bbox = new Envelope(new Coordinate(this.bbox.getXmin(), this.bbox.getYmin()), new Coordinate(coordinate.getX(), this.bbox.getYmax()));
+			}
+			if(coordinate.getX() < this.bbox.getXmax() && coordinate.getY() > this.bbox.getYmax()) {
+				this.bbox = new Envelope(new Coordinate(this.bbox.getXmin(), this.bbox.getYmin()), new Coordinate(this.bbox.getXmax(), coordinate.getY()));
+			}
+		}
+	}
+	
+	public Envelope build() {
+		return this.bbox;
+	}
+}
