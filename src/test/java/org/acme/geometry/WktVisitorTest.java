edfcbfbbb22c9d160c80a1dd2c7dc2d60f59663c
Brun04 <bruno.verchere04@orange.fr>
2021-01-05 10:26:15 +0100

    Geometry as text with abstract geometry (0.11)

--- src/test/java/org/acme/geometry/WktVisitorTest.java
+++ src/test/java/org/acme/geometry/WktVisitorTest.java
@@ -7,33 +7,33 @@ public class WktVisitorTest {
 
 	@Test
 	public void testVisitPoint() {
-		WktVisitor visitor = new WktVisitor();
-		Geometry g = new Point(new Coordinate(3.0,4.0));
-		g.accept(visitor);
-		Assert.assertEquals("POINT(3.0 4.0)", visitor.getResult());
+		//WktVisitor visitor = new WktVisitor();
+		AbstractGeometry g = new Point(new Coordinate(3.0,4.0));
+		//g.accept(visitor);
+		Assert.assertEquals("POINT(3.0 4.0)", g.asText());
 	}
 	
 	@Test
 	public void testVisitEmptyPoint() {
-		WktVisitor visitor = new WktVisitor();
-		Geometry g = new Point();
-		g.accept(visitor);
-		Assert.assertEquals("POINT EMPTY", visitor.getResult());
+		//WktVisitor visitor = new WktVisitor();
+		AbstractGeometry g = new Point();
+		//g.accept(visitor);
+		Assert.assertEquals("POINT EMPTY", g.asText());
 	}
 	
 	@Test
 	public void testWriteLineString() {
-		WktVisitor visitor = new WktVisitor();
-		Geometry g = SampleFactory.createLineStringMN();
-		g.accept(visitor);
-		Assert.assertEquals("LINESTRING(1.0 -2.5,0.0 3.0)", visitor.getResult());
+		//WktVisitor visitor = new WktVisitor();
+		AbstractGeometry g = SampleFactory.createLineStringMN();
+		//g.accept(visitor);
+		Assert.assertEquals("LINESTRING(1.0 -2.5,0.0 3.0)", g.asText());
 	}
 	
 	@Test
 	public void testWriteEmptyLineString() {
-		WktVisitor visitor = new WktVisitor();
-		Geometry g = new LineString();
-		g.accept(visitor);
-		Assert.assertEquals("LINESTRING EMPTY", visitor.getResult());
+		//WktVisitor visitor = new WktVisitor();
+		AbstractGeometry g = new LineString();
+		//g.accept(visitor);
+		Assert.assertEquals("LINESTRING EMPTY", g.asText());
 	}
 }
