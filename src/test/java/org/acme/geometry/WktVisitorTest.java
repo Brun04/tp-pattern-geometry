48ffc26c4803b5b9fbe792d5d8bf7ddf8dbe3624
Brun04 <bruno.verchere04@orange.fr>
2021-01-05 16:20:15 +0100

    Remove unused comments

--- src/test/java/org/acme/geometry/WktVisitorTest.java
+++ src/test/java/org/acme/geometry/WktVisitorTest.java
@@ -15,25 +15,19 @@ public class WktVisitorTest {
 	
 	@Test
 	public void testVisitEmptyPoint() {
-		//WktVisitor visitor = new WktVisitor();
 		AbstractGeometry g = new Point();
-		//g.accept(visitor);
 		Assert.assertEquals("POINT EMPTY", g.asText());
 	}
 	
 	@Test
 	public void testWriteLineString() {
-		//WktVisitor visitor = new WktVisitor();
 		AbstractGeometry g = SampleFactory.createLineStringMN();
-		//g.accept(visitor);
 		Assert.assertEquals("LINESTRING(1.0 -2.5,0.0 3.0)", g.asText());
 	}
 	
 	@Test
 	public void testWriteEmptyLineString() {
-		//WktVisitor visitor = new WktVisitor();
 		AbstractGeometry g = new LineString();
-		//g.accept(visitor);
 		Assert.assertEquals("LINESTRING EMPTY", g.asText());
 	}
 	
